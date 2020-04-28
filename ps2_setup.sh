01782ddf2f1c0e5d4345336d3eb7f3e9cf356099
Minh-An Dao <minhan7497@gmail.com>
2020-04-28 11:18:18 +0700

    update souce code

---
+++ ps2_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eu -o pipefail # fail on error , debug all lines
+
+if [ 'root' != $( whoami ) ] ; then
+  echo "Please run as root! ( sudo ${0} )"
+  exit 1;
+fi
+
+echo "This will set up the PS2 Controller's prequisites for Raspberry Pi"
+
+# workaround for automatically calling from main shell script
+if ! [ "$1" == "ignore" ] ; then
+
+	apt-get update
+	apt-get upgrade -y
+
+fi
+
+# WiringPi  ## http://wiringpi.com/
+apt-get install wiringpi -y
+
+# ----------- renew binary files --------------------
+chmod +x ps2_bin_reset.sh
+./ps2_bin_reset.sh
+
+echo
+echo
+echo "PS2 Done."
+
+## (c) 2020 Minh-An Dao. All right reserved
